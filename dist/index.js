156c6ac0ae1ec47f3087025c8ca8a81938b61d9d
mustafa.oguz <m.sabri.oguz@gmail.com>
2024-11-07 16:30:42 +0300

    Güncelleme

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-console.log('Hello, World!');
-var a = 1;
+var a = "sa";
+var b = 1;
+var c = true;
+var d = undefined;
+var e = Symbol("a");
+var myDizi = ["a", "b", 1];
+var a1 = myDizi[0], b1 = myDizi[1], c1 = myDizi[2];
+console.log(a1);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-console.log('Hello, World!');
+let a: string = "sa";
+let b: number = 1;
+let c: boolean = true;
+let d: undefined = undefined;
+let e: symbol = Symbol("a");
 
-const a = 1;
+const myDizi: [string, string, number] = ["a", "b", 1];
+
+const [a1, b1, c1] = myDizi;
+
+console.log(a1);
