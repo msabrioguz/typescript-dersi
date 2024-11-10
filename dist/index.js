bacc6b3509af7f22030dcb2cd2f29b406b2812ef
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2024-11-10 08:42:12 +0300

    Type Aliasing (Tip İsimlendirme)

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,25 @@
-function isimYaz(isim, tekrar) {
-    for (var i = 0; i < tekrar; i++) {
-        console.log("".concat(i + 1, ". sat\u0131r: "), isim);
-    }
-}
-isimYaz("ali", 50);
+"use strict";
+var myColor = "blue";
+var colorArray = ["red", "green", "blue", "cyan", "magenta", "turquoise"];
+var allColors = ["red", "green", "blue", "red"];
+var dogPerson = {
+    isim: "Zeynep",
+    soyisim: "Yılmaz",
+    yas: 10,
+    cins: "Golden",
+    tur: "Husky",
+};
+var dogPerson1 = {
+    isim: "Mustafa",
+    soyisim: "Oğuz",
+    yas: 35,
+    cins: "Köpek",
+    tur: "Golden"
+};
+var myMap = new Map();
+myMap.set('kadir', {
+    isim: 'Kadir',
+    soyisim: 'Çalışkan',
+    yas: 25
+});
+console.log(myMap);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,53 @@
-function isimYaz(isim: string, tekrar: number): void {
-    for (let i = 0; i < tekrar; i++){
-        console.log(`${i+1}. satır: `,isim);
-    }
+type Colors = 'red' | 'green' | 'blue';
+
+type CustomColors = 'magenta' | 'cyan' | 'turquoise';
+
+type AllColors = Colors | CustomColors;
+
+const myColor: Colors = "blue";
+
+const colorArray: (Colors | CustomColors)[] = ["red", "green", "blue", "cyan", "magenta", "turquoise"];
+
+const allColors: AllColors[] = ["red","green", "blue", "red"];
+
+type Person = {
+    isim: string;
+    soyisim: string;
+    yas: number;
 }
 
-isimYaz("ali", 50);
+type Dog = {
+    yas:number;
+    cins: string;
+    tur: string;
+}
+
+type DogPerson = Person & Dog;
+
+const dogPerson: DogPerson = {
+    isim: "Zeynep",
+    soyisim: "Yılmaz",
+    yas: 10,
+    cins: "Golden",
+    tur: "Husky",
+}
+
+const dogPerson1: DogPerson = {
+    isim: "Mustafa",
+    soyisim: "Oğuz",
+    yas: 35,
+    cins: "Köpek",
+    tur: "Golden"
+}
+
+type PersonMap = Map<string, Person>;
+
+const myMap: PersonMap = new Map();
+
+myMap.set('kadir', {
+    isim: 'Kadir',
+    soyisim: 'Çalışkan',
+    yas: 25
+});
+
+console.log(myMap);
